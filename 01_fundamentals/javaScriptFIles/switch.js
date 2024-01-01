b5d2c0bf24629ae80f87a56c50112a973df7a3bf
NarayanDhakal121 <141776158+NarayanDhakal121@users.noreply.github.com>
2024-01-01 11:24:27 +0000

    updates on switch

--- 01_fundamentals/javaScriptFIles/switch.js
+++ 01_fundamentals/javaScriptFIles/switch.js
@@ -18,19 +18,19 @@
 
 // tasks
 
-// let browser = prompt("enter browser name")
+let browser = prompt("enter browser name")
 
-// if (browser == 'Edge') {
-//     alert( "You've got the Edge!" );
-//   } else if (browser == 'Chrome' || browser == 'Firefox' || browser == 'Safari' || browser == 'Opera') {
-//     alert( 'Okay we support these browsers too' );
-//   } else {
-//     alert( 'We hope that this page looks ok!' );
-//   }
-
-
+if (browser == 'Edge') {
+    alert( "You've got the Edge!" );
+  } else if (browser == 'Chrome' || browser == 'Firefox' || browser == 'Safari' || browser == 'Opera') {
+    alert( 'Okay we support these browsers too' );
+  } else {
+    alert( 'We hope that this page looks ok!' );
+  }
 
 
+
+  
   let a = +prompt('a?', '');
 
 
