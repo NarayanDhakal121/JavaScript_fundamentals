cba3da2a8f7a2800175eda6c4633b45c29c19c46
NarayanDhakal121 <141776158+NarayanDhakal121@users.noreply.github.com>
2024-01-01 12:24:57 +0000

    functions expression, and the arrow functions

---
+++ 01_fundamentals/javaScriptFIles/functions.js
@@ -0,0 +1,305 @@
+// javaScript functions
+
+// function showMessage() {
+//     alert( 'Hello everyone!' );
+//   }
+
+
+//   function showMessage() {
+//     let message = "Hello, I'm JavaScript!"; 
+  
+//     alert( message );
+//   }
+  
+//   showMessage();
+  
+//   alert( message ); 
+
+// let userName = 'John';
+
+// function showMessage() {
+//   let message = 'Hello, ' + userName;
+//   alert(message);
+// }
+
+// showMessage();
+
+// let userName = 'John';
+
+// function showMessage() {
+//   userName = "Bob";
+
+//   let message = 'Hello, ' + userName;
+//   alert(message);
+// }
+
+// alert( userName ); 
+// showMessage();
+
+// alert( userName );
+
+
+
+//parameters in functions
+// function showMessage(from, text) {
+//     alert(from + ': ' + text);
+//   }
+  
+//   showMessage('Ann', 'Hello!'); 
+//   showMessage('Ann', "What's up?"); 
+
+// function showMessage(from, text) {
+
+//     from = '*' + from + '*';
+//   }
+  
+//   let from = "Ann";
+  
+//   showMessage(from, "Hello"); 
+//   alert( from );
+
+// function showMessage(from, text = "no text given") {
+//     alert( from + ": " + text );
+//   }
+  
+//   showMessage("Ann");
+
+// use of operators
+
+// function showMessage(text) {
+//     text = text || 'empty';
+//     alert(text);
+//   }
+  
+//   showMessage('hello');
+  
+
+//use of nullish coalescing operator ?? in function
+// function showCount(count) {
+   
+//     alert(count ?? "unknown");
+//   }
+  
+//   showCount(0); 
+//   showCount(null);
+//   showCount();
+
+
+//return a value
+
+// function sum(a, b) {
+//     return a + b;
+//   }
+  
+//   let result = sum(1, 2);
+//   alert( result );
+  
+
+// function checkAge(age) {
+//     if (age >= 18) {
+//       return true;
+//     } else {
+//       return confirm('Do you have permission from your parents?');
+//     }
+//   }
+  
+//   let age = prompt('How old are you?', 18);
+  
+//   if ( checkAge(age) ) {
+//     alert( 'Access granted' );
+//   } else {
+//     alert( 'Access denied' );
+//   }
+
+//function to check prime numbers
+
+// function showPrimes(n) {
+//     nextPrime: for (let i = 2; i < n; i++) {
+  
+//       for (let j = 2; j < i; j++) {
+//         if (i % j == 0) continue nextPrime;
+//       }
+  
+//       alert( i ); 
+//     }
+//   }
+
+// function showPrimes(n) {
+
+//     for (let i = 2; i < n; i++) {
+//       if (!isPrime(i)) continue;
+  
+//       alert(i);  
+//     }
+//   }
+  
+//   function isPrime(n) {
+//     for (let i = 2; i < n; i++) {
+//       if ( n % i == 0) return false;
+//     }
+//     return true;
+//   }
+
+
+
+// tasks of function
+
+// let age = 15;
+
+// function checkAge(age) {
+//   if (age > 18) {
+//     return true;
+//   } else {
+//     return confirm('Did parents allow you?');
+//   }
+// }
+
+// let result = checkAge(age);
+// alert(result);
+
+// let age=20;
+
+// function checkAge(age) {
+//     return (age > 18) ? true : confirm('Did parents allow you?');
+//   }
+//   let result = checkAge(age);
+//   alert(result);
+
+// let age = 10;
+
+// function checkAge(age) {
+//   return (age > 18) || confirm('Did parents allow you?');
+// }
+
+// let result = checkAge(age);
+// alert(result);
+
+
+// function min(a, b) {
+//     if (a < b) {
+//       return a;
+//     } else {
+//       return b;
+//     }
+//   }
+
+// let number1 = 10;
+// let number2 = 20;
+
+// let minimum = min(number1, number2);
+
+// console.log(`The minimum of ${number1} and ${number2} is ${minimum}.`);
+
+// function pow(x, n) {
+//     let result = x;
+  
+//     for (let i = 1; i < n; i++) {
+//       result *= x;
+//     }
+  
+//     return result;
+//   }
+  
+//   let x = prompt("x?", '');
+//   let n = prompt("n?", '');
+  
+//   if (n < 1) {
+//     alert(`Power ${n} is not supported, use a positive integer`);
+//   } else {
+//     alert( pow(x, n) );
+//   }
+
+
+// function expression
+
+// function sayHi() {  
+//     alert( "Hello" );
+//   }
+  
+//   let func = sayHi;    
+  
+//   func(); 
+//   sayHi();
+
+// function ask(question, yes, no) {
+//     if (confirm(question)) yes()
+//     else no();
+//   }
+  
+//   function showOk() {
+//     alert( "You agreed." );
+//   }
+  
+//   function showCancel() {
+//     alert( "You canceled the execution." );
+//   }
+  
+//   ask("Do you agree?", showOk, showCancel);
+
+// sayHi("John");
+
+// function sayHi(name) {
+//   alert( `Hello, ${name}` );
+// }
+
+
+// let age = prompt("What is your age?", 18);
+
+// if (age < 18) {
+
+//   function welcome() {
+//     alert("Hello!");
+//   }
+
+// } else {
+
+//   function welcome() {
+//     alert("Greetings!");
+//   }
+
+// }
+// welcome();
+
+// Arrow functions
+
+// let sum = (a, b) => a + b;
+
+
+// let sum = function(a, b) {
+//   return a + b;
+// };
+
+
+// alert( sum(1, 2) ); 
+
+// let double = n => n * 2;
+// alert( double(3) );
+
+// let age = prompt("What is your age?", 18);
+
+// let welcome = (age < 18) ?
+//   () => alert('Hello!') :
+//   () => alert("Greetings!");
+
+// welcome();
+
+//multiline arrow function
+
+// let sum = (a, b) => {  
+//     return result; 
+//   };
+  
+//   alert( sum(1, 2) );
+
+  //Taks
+
+//   function ask(question, yes, no) {
+//     if (confirm(question)) yes();
+//     else no();
+//   }
+  
+//   ask(
+//     "Do you agree?",
+//     () => alert("You agreed."),
+//     () => alert("You canceled the execution.")
+//   );
