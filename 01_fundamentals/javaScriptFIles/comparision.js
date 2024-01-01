fd5620624a1af8ea29d293ee0b098412c64eeb72
NarayanDhakal121 <141776158+NarayanDhakal121@users.noreply.github.com>
2024-01-01 09:53:04 +0000

    done

--- 01_fundamentals/javaScriptFIles/comparision.js
+++ 01_fundamentals/javaScriptFIles/comparision.js
@@ -1,1 +1,38 @@
-alert( 2 > 1 );
+// alert( 2 > 1 );
+// alert( 2 == 1 );
+// alert( 2 != 1 );
+
+// alert( 'Z' > 'A' );
+
+// alert( '2' > 1 );
+// alert( '01' == 1 );
+
+// alert( true == 1 );
+// alert( false == 0 );
+
+// let a = 0;
+// alert( Boolean(a) ); 
+
+// let b = "0";
+// alert( Boolean(b) ); 
+
+// alert(a == b);
+
+
+// striict equality
+
+// alert( 0 == false );
+// alert( 0 === false )
+
+// alert( null == undefined );
+
+
+
+// tasks
+alert(5 > 4);
+alert("apple" > "pineapple")
+alert("2" > "12")
+alert(undefined == null)
+alert(undefined === null)
+alert(null == "\n0\n")
+alert(null === +"\n0\n")
