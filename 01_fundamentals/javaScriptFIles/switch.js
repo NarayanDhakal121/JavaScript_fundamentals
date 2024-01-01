defe557a995dd2d32dd2e2da30552a54a9913b3b
NarayanDhakal121 <141776158+NarayanDhakal121@users.noreply.github.com>
2024-01-01 11:21:20 +0000

    switch completed

---
+++ 01_fundamentals/javaScriptFIles/switch.js
@@ -0,0 +1,50 @@
+// switch statement
+
+// let a = 60+ 2;
+
+// switch (a) {
+//   case 3:
+//     alert( 'Too small' );
+//     break;
+//   case 4:
+//     alert( 'Exactly!' );
+//     break;
+//   case 5:
+//     alert( 'Too big' );
+//     break;
+//   default:
+//     alert( "I don't know such values" );
+// }
+
+// tasks
+
+// let browser = prompt("enter browser name")
+
+// if (browser == 'Edge') {
+//     alert( "You've got the Edge!" );
+//   } else if (browser == 'Chrome' || browser == 'Firefox' || browser == 'Safari' || browser == 'Opera') {
+//     alert( 'Okay we support these browsers too' );
+//   } else {
+//     alert( 'We hope that this page looks ok!' );
+//   }
+
+
+
+
+  let a = +prompt('a?', '');
+
+
+switch (a) {
+  case 0:
+    alert( 0 );
+    break;
+
+  case 1:
+    alert( 1 );
+    break;
+
+  case 2:
+  case 3:
+    alert( '2,3' );
+    break;
+}
