5994d9d8d71494023e84ab6bfb8d37718020bc80
NarayanDhakal121 <141776158+NarayanDhakal121@users.noreply.github.com>
2024-01-02 11:15:48 +0000

    methods of primitives and task

--- Arrays/arrays.js
+++ Arrays/arrays.js
@@ -1,2 +1,28 @@
+//methods of primitives
 
-// empty 
+// let john = {
+//     name: "John",
+//     sayHi: function() {
+//       alert("Hi buddy!");
+//     }
+//   };
+  
+//   john.sayHi();
+
+
+//A primitavive as an object
+
+let str1 = "Hello";
+
+alert( str1.toUpperCase() );
+
+
+
+let str = "Hello";
+
+str.test = 5; 
+
+alert(str.test);
+
+// null or unddefined
+alert(null.test); 
