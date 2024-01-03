64bffb2aecaae0edeeaa4bb84b3b3ac2bb13fd1f
NarayanDhakal121 <141776158+NarayanDhakal121@users.noreply.github.com>
2024-01-03 06:22:47 +0000

    updates on arrays

--- 01_fundamentals/Arrays/arrays.js
+++ 01_fundamentals/Arrays/arrays.js
@@ -241,3 +241,164 @@ let army = {
   alert(soldiers[0].age); 
   alert(soldiers[1].age); 
 
+// .......Tasks..........
+
+// let fruits = ["Apples", "Pear", "Orange"];
+// let shoppingCart = fruits;
+// shoppingCart.push("Banana");
+// alert( fruits.length );
+
+//......tasks........
+let styles = ["Jazz", "Blues"];
+styles.push("Rock-n-Roll");
+styles[Math.floor((styles.length - 1) / 2)] = "Classics";
+alert( styles.shift() );
+styles.unshift("Rap", "Reggae");
+
+//.......tasks..........
+
+let arr = ["a", "b"];
+
+arr.push(function() {
+  alert( this );
+});
+
+arr[2]();
+
+// ........tasks.......
+
+function sumInput() {
+
+  let numbers = [];
+
+  while (true) {
+
+    let value = prompt("A number please?", 0);
+    if (value === "" || value === null || !isFinite(value)) break;
+    numbers.push(+value);
+  }
+
+  let sum = 0;
+  for (let number of numbers) {
+    sum += number;
+  }
+  return sum;
+}
+
+alert( sumInput() );
+
+// .......tasks................
+
+function getMaxSubSum(arr) {
+  let maxSum = 0;
+  let partialSum = 0;
+
+  for (let item of arr) {
+    partialSum += item;
+    maxSum = Math.max(maxSum, partialSum); 
+    if (partialSum < 0) partialSum = 0; 
+  }
+
+  return maxSum;
+}
+
+alert( getMaxSubSum([-1, 2, 3, -9])); 
+alert( getMaxSubSum([-1, 2, 3, -9, 11])); 
+alert( getMaxSubSum([-2, -1, 1, 2])); 
+alert( getMaxSubSum([100, -9, 2, -3, 5])); 
+alert( getMaxSubSum([1, 2, 3])); 
+alert( getMaxSubSum([-1, -2, -3])); 
+
+
+//transform an array
+
+let countries = ['india', 'China', 'Nepal'];
+alert( countries.sort( (a, b) => a > b ? 1 : -1));
+alert( countries.sort( (a, b) => a.localeCompare(b))); 
+
+//Array.isArray
+alert(typeof {});
+alert(typeof []);
+
+
+//....tasks.....
+
+// function filterRange(arr, a, b) {
+//   return arr.filter(item => (a <= item && item <= b));
+// }
+
+// let arr = [5, 3, 8, 1];
+// let filtered = filterRange(arr, 1, 4);
+// alert( filtered ); 
+// alert( arr );
+
+//.......tasks
+
+function filterRangeInPlace(arr, a, b) {
+
+  for (let i = 0; i < arr.length; i++) {
+    let val = arr[i];
+    if (val < a || val > b) {
+      arr.splice(i, 1);
+      i--;
+    }
+  }
+
+}
+
+let arr = [5, 3, 8, 1];
+
+filterRangeInPlace(arr, 1, 4); 
+
+alert( arr ); 
+
+
+//.......tasks..........
+
+// let arr = [5, 2, 1, -10, 8];
+
+// arr.sort((a, b) => b - a);
+
+// alert( arr );
+
+
+// tasks
+
+function copySorted(arr) {
+  return arr.slice().sort();
+}
+
+let arrey = ["HTML", "JavaScript", "CSS"];
+let sorted = copySorted(arrey);
+
+alert( sorted );
+alert( arrey );
+
+
+// tasks
+
+let john = { name: "John", surname: "Smith", id: 1 };
+let pete = { name: "Pete", surname: "Hunt", id: 2 };
+let mary = { name: "Mary", surname: "Key", id: 3 };
+
+let users1 = [ john, pete, mary ];
+
+let usersMapped = users1.map(user => ({
+  fullName: `${user.name} ${user.surname}`,
+  id: user.id
+}));
+
+alert( usersMapped[0].id );
+alert( usersMapped[0].fullName );
+
+
+//........Tasks.....
+function shuffle(array) {
+  array.sort(() => Math.random() - 0.5);
+}
+
+let arr = [1, 2, 3];
+shuffle(arr);
+alert(arr);
+
+
